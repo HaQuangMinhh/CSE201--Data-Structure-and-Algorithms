87a0448edc6c98092c75d072d27c8ca08f44cdf4
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2025-04-06 17:38:32 +0700

    ver2 100d

--- how to resolve/EIPAGES.java
+++ how to resolve/EIPAGES.java
@@ -1,101 +1,58 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class EIPAGES {
 
-	
-	static InputReader reader = new InputReader(System.in);
+    static StringBuilder sb = new StringBuilder(); 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		StringBuilder sb = new StringBuilder();
-		
-		int n = reader.nextInt();
-		
-		int[] numberPages = new int[n];
-		
-		for (int i = 0 ; i < numberPages.length; i++) {
-			numberPages[i] = reader.nextInt();  // cho users input 
-		}
-		
-		Arrays.sort(numberPages);
-		
-		for (int i = 0 ; i < n ; i++) {
-			int start = numberPages[i];
-			sb.append(start);
-			
-			int end = start;
-			
-			int count = 0;
-			while (i + 1 < n && numberPages[i +1] - numberPages[i] == 1) {
-				end = numberPages[i + 1];
-				i++;
-				count++;
-			}
-			if ( count >= 2) {
-				sb.append("-").append(end);
-			} else if (count == 1) {
-				sb.append(" ").append(end);
-			}
-			
-			sb.append(" ");
-		}
-		System.out.println(sb);
-	}
-	
-	
-	
-	
-	static class InputReader {
-        StringTokenizer tokenizer;
-        BufferedReader reader;
-        String token;
-        String temp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
 
-        public InputReader(InputStream stream) {
-            tokenizer = null;
-            reader = new BufferedReader(new InputStreamReader(stream));
+        int num = sc.nextInt();
+
+        List<Integer> numPages = new ArrayList<>(); 
+
+        for ( int i = 0 ; i < num ; i++ ) {
+            var result = sc.nextInt();
+            numPages.add(result);
         }
 
-        public InputReader(FileInputStream stream) {
-            tokenizer = null;
-            reader = new BufferedReader(new InputStreamReader(stream));
-        }
+        var result = compressPages(numPages);
+        System.out.println(result);
 
-        public String nextLine() throws IOException {
-            return reader.readLine();
-        }
-
-        public String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    if (temp != null) {
-                        tokenizer = new StringTokenizer(temp);
-                        temp = null;
-                    } else {
-                        tokenizer = new StringTokenizer(reader.readLine());
-                    }
-                } catch (IOException e) {
-                }
-            }
-            return tokenizer.nextToken();
-        }
-
-        public double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
+        sc.close();
     }
+
+    public static String compressPages ( List<Integer> pages ) {
+
+        Collections.sort(pages);
+
+        for ( int i = 0 ; i < pages.size() ; i++ ) {
+            int start = pages.get(i);  // Trang đầu tiên 
+            sb.append(start);
+
+            int end = start ; 
+
+            int count = 0 ; 
+
+            while ( i + 1 < pages.size() && pages.get(i + 1) - pages.get(i) == 1 ) {
+                end = pages.get(i + 1);
+                count++;
+                i++ ; 
+            }
+
+            if ( count >= 2 ) {
+                sb.append("-").append(end);
+            } else if ( count == 1 ) {
+                sb.append(" ").append(end);
+            }
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+
 }
